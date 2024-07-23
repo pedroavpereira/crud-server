950a106f2516be514690f600d59522987c834ca9
Pedro pereira <pedroavpereira2000@gmail.com>
2024-07-23 10:18:25 +0100

    Add CORS

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const fruitRouter = require("./routes/fruitRouter");
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
